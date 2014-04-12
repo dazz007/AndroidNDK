ac7ba9a984ea2bb6f4ab140561259f45c57ea849
Paweł Kędzia <madaf007@gmail.com>
2014-04-13 00:45:20 +0200

    Szkielet aplikacji

---
+++ AndroidAKE/obj/local/armeabi/objs/nativecryptowrapper/nativecryptowrapper/MACWrapper.o.d
@@ -0,0 +1,58 @@
+obj/local/armeabi/objs/nativecryptowrapper/nativecryptowrapper/MACWrapper.o: \
+ jni/nativecryptowrapper/MACWrapper.cpp jni/include/MACWrapper.h \
+ C:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include/jni.h \
+ C:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include/sys/cdefs.h \
+ C:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include/sys/cdefs_elf.h \
+ C:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include/android/api-level.h \
+ C:/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stdarg.h \
+ C:/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/_cprolog.h \
+ C:/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/features.h \
+ C:/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h \
+ C:/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/user_config.h \
+ C:/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/compat.h \
+ C:/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/host.h \
+ C:/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/_system.h \
+ C:/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/_android.h \
+ C:/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h \
+ C:/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h \
+ C:/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h \
+ C:/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/_epilog.h \
+ C:/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h
+
+jni/include/MACWrapper.h:
+
+C:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include/jni.h:
+
+C:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include/sys/cdefs.h:
+
+C:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include/sys/cdefs_elf.h:
+
+C:/android-ndk-r9d/platforms/android-18/arch-arm/usr/include/android/api-level.h:
+
+C:/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stdarg.h:
+
+C:/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/_cprolog.h:
+
+C:/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/features.h:
+
+C:/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h:
+
+C:/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/user_config.h:
+
+C:/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/compat.h:
+
+C:/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/host.h:
+
+C:/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/_system.h:
+
+C:/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/_android.h:
+
+C:/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h:
+
+C:/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h:
+
+C:/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h:
+
+C:/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/_epilog.h:
+
+C:/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h:
